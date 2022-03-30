d38629c4c268cb64037711414fa6bba04a362297
stevari <villevartia@hotmail.com>
2022-03-30 15:44:37 +0300

    Vakioarvot päivitetty

--- Vakiot.js
+++ Vakiot.js
@@ -1,5 +1,5 @@
 /*Vakioarvoiset muuttujat joita käytetään mm. pelikentän ja palikoiden renderöimiseen
 määritellään täällä */
 const SARAKKEET = 10; //Pelikenttä on mitoiltaan 10 x 20
-const RIVIT = 18;
+const RIVIT = 20;
 const BLOKIN_KOKO = 30; //Määrittää palikan koon
