d31592e25d9cdcca87eb1e3fae622d5e3a536df5
stevari <villevartia@hotmail.com>
2022-04-07 19:49:28 +0300

    kenttä luokkaa viety pidemälle uusilla funktioilla. Käytännössä osa pelimoottorin logiikasta siiretty kenttä luokalle

--- Kentta.js
+++ Kentta.js
@@ -6,16 +6,31 @@ Tyhjää solua merkitään numerolla 0, värejä numeroilla 1-7
 */
 
 class Kentta {
-  
-    
-    reset() { //Resetoi kentän
-      this.grid = this.getEmptyKentta();
-    }
-    
-    
-    getEmptyKentta() { //Luo matriisin joka koostuu tyhjistä soluista
-      return Array.from(
-        {length: RIVIT}, () => Array(SARAKKEET).fill(0)
-      );
-    }
+  ctx;
+  grid;
+  palikka;
+
+  constructor(ctx) { //construktori saa ctx:n referenssiksi
+    this.ctx = ctx;
+    this.alusta(); //alustetaan kenttä
+
   }
+  alusta() {
+    ctx.canvas.width = SARAKKEET * BLOKIN_KOKO; //Määritellään canvas vakioarvoilla
+    ctx.canvas.height = RIVIT * BLOKIN_KOKO;
+    ctx.scale(BLOKIN_KOKO, BLOKIN_KOKO); //Skaalataan konteksti jotta palikan mitat sopii kentän mittoihin
+  }
+
+  reset() { //Resetoi kentän ja luo palikan
+    this.grid = this.getEmptyKentta();
+    this.palikka = new Palikka(ctx);
+    this.palikka.piirraPalikka();
+
+  }
+
+  getEmptyKentta() { //Luo matriisin joka koostuu tyhjistä soluista
+    return Array.from(
+      { length: RIVIT }, () => Array(SARAKKEET).fill(0)
+    );
+  }
+}
