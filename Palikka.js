b7cbe102d4d149185dc3246e813e5db11ea86267
stevari <villevartia@hotmail.com>
2022-04-07 19:51:33 +0300

    palikka olio hakee nyt värin ja muodon vakiossa määriteltyistä arvoista. Lisäksi palikalla on nyt tyyppi

--- Palikka.js
+++ Palikka.js
@@ -12,47 +12,57 @@ esim.  L voisi olla
 
 jossa 2 tarkoittaa tiettyä värinumeroa.
 
+
 */
 
 
-class Palikka {  
-    constructor(ctx) { 
-        /*
-        Palikka -olion täytyy tietää missä se on suhteessa pelikenttään ja minkä värinen ja
-        muotoinen se on.
-        Näitä tietoja varten se saa parametrikseen canvasin contextin tiedot. 
-        */
-      this.ctx = ctx; 
-      this.color = 'blue';
-      this.shape = [ 
-        [0, 0, 0], 
-        [2, 2, 2], 
-        [2, 0, 0]
-      ];
-      
-      
-      this.x = 3; //aloituspositio canvasilla. (3,-1) = keskellä ylhäällä. (0,0) olisi kentän vasen yläreuna.
-      this.y = -1;  
-    }
+class Palikka {
+  ctx;
+  x;
+  y;
+  color;
+  shape;
+  tyyppiId;
 
 
-    piirraPalikka() {
-        this.ctx.fillStyle = this.color; //väritetään haluamamme kohdat valitulla värillä
-        this.shape.forEach((rivi, y) => { //loopataan matriisin sarakkeet ja rivit etsien soluja, joiden arvo poikkeaa nollasta
-          rivi.forEach((arvo, x) => {
-            /*
-             (this.x, this.y) on palikan vasen ylänurkka.
-             
-
-             Jos solun arvo on erisuuri kuin 0, ts. jos solu ei ole tyhjä, se täytetään valitulla värillä.
-             Näin palikka piirtyy kentälle.
-            */
-            if (arvo > 0) { 
-              this.ctx.fillRect(this.x + x, this.y + y, 1, 1); 
-            } 
-          });
-        });
-      }
+  constructor(ctx) {
+    /*
+    Palikka -olion täytyy tietää missä se on suhteessa pelikenttään ja minkä värinen ja
+    muotoinen se on.
+    Näitä tietoja varten se saa parametrikseen canvasin contextin tiedot. 
+    */
+    this.ctx = ctx;
+    this.spawnaa();
+  }
+  spawnaa() {
+    this.tyyppiId = this.randomTetromiinoTyyppi(VARIT.length - 1)
+    this.shape = MUODOT[this.tyyppiId]
+    this.color = VARIT[this.tyyppiId]
+    this.x = 0
+    this.y = 0
 
   }
-  
+  randomTetromiinoTyyppi(noOfTypes) {
+    return Math.floor(Math.random() * noOfTypes + 1);
+  }
+
+
+  piirraPalikka() {
+    this.ctx.fillStyle = this.color; //väritetään haluamamme kohdat valitulla värillä
+    this.shape.forEach((rivi, y) => { //loopataan matriisin sarakkeet ja rivit etsien soluja, joiden arvo poikkeaa nollasta
+      rivi.forEach((arvo, x) => {
+        /*
+         (this.x, this.y) on palikan vasen ylänurkka.
+         
+
+         Jos solun arvo on erisuuri kuin 0, ts. jos solu ei ole tyhjä, se täytetään valitulla värillä.
+         Näin palikka piirtyy kentälle.
+        */
+        if (arvo > 0) {
+          this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+        }
+      });
+    });
+  }
+
+}
