90e2bc5d601bb5c0261fef8303a83701423c0f96
stevari <villevartia@hotmail.com>
2022-04-19 19:03:05 +0300

    koodin siivousta

--- Kentta.js
+++ Kentta.js
@@ -33,13 +33,20 @@ class Kentta {
     this.piirraKentta();
   }
 
-  pudotaPalikka() {
-    if(this.palikka.validiSiirto("alas")){
-      this.palikka.liiku("alas");
-    }else{
+  pudotaPalikka() { 
+    let p = liikkeet[KEY.ALAS](this.palikka);
+    if (this.valid(p)) {
+      this.palikka.liiku(p);
+    } else {
       this.liimaaPalikkaKentalle();
+      
+      if (this.palikka.y === 0) {
+        // Peli loppui
+        return false;
+      }
+     this.luoUusiPalikka();
     }
-    
+    return true;
   }
 
   liimaaPalikkaKentalle() {
@@ -62,7 +69,45 @@ class Kentta {
       });
     });
   }
+  seinanSisalla(x) {
+    return x >= 0 && x < SARAKKEET; //jos x < 0, palikka on vasemman seinän sisällä. jos taas x > SARAKKAAT (10), x on oikean seinän sisällä
+  }
 
+  pohjanYlapuolella(y) {
+    return y <= RIVIT; //tarkistetaan, onko palikan y-arvo pohjan (RIVIT = 20) yläpuolella. Palikka ei saa mennä pohjasta läpi (y>20)
+  }
+
+  eiVarattu(x, y) {
+    return this.grid[y] && this.grid[y][x] === 0; //tarkistetaan, onko kentän paikalla jo jotain, esim toinen palikka. jos on, arvot != 0, koska ne ovat väritetty
+  }
+
+  valid(p) { 
+    return p.shape.every((row, dy) => { //käydään palikan (matriisin) jokainen arvo läpi tarkistaen, että seuraava (muutettu) arvo on validi
+      return row.every((value, dx) => {
+        let x = p.x + dx;
+        let y = p.y + dy;
+        return (
+          value === 0 ||
+          (this.seinanSisalla(x) && this.pohjanYlapuolella(y) && this.eiVarattu(x, y))
+        );
+      });
+    });
+    
+  }
+
+  rotate(palikka) {
+    //JSON.stringify() luo palikka -oliosta string -olion, jolla on palikan tiedot.
+    //JSON.parse() muuntaa string olion takaisin palikka -olioksi
+    //Kääntää palikan, eli matriisin 90 
+    let klooni = JSON.parse(JSON.stringify(palikka)); 
+    for (let y = 0; y < klooni.shape.length; ++y) {
+      for (let x = 0; x < y; ++x) {
+        [klooni.shape[x][y], klooni.shape[y][x]] = [klooni.shape[y][x], klooni.shape[x][y]]; //rivit sarakkeiksi ja päinvastoin
+      }
+    }
+    klooni.shape.forEach(row => row.reverse());
+    return klooni;
+  }
 
   luoUusiPalikka() {
     this.palikka = new Palikka(ctx);
