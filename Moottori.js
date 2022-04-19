33a68fe71ae2be3c3f463e1c6b3977b0ea85b7dc
stevari <villevartia@hotmail.com>
2022-04-19 19:03:35 +0300

    hieman tviikkauksia logiikkaan

--- Moottori.js
+++ Moottori.js
@@ -5,77 +5,62 @@ sekä lopettaa pelisession.
 const canvas = document.getElementById("kentta"); //haetaan pohjaksi kenttä, joka alustettiin html tiedostossa
 const ctx = canvas.getContext("2d");
 const kentta = new Kentta(ctx); //luodaan uusi kenttä
-const time = { start: 0, elapsed: 0, level: 1000 };
+const time = { start: 0, elapsed: 0, level: 1000 }; //alustetaan aika
+
+//console.log(MUODOT)
 
 function clear() {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); //poistaa vanhan instanssin
 }
+
 let requestId;
 
 function animate(now = 0) { //toimii looppina, mahdollistaa mm. palikan jatkuvan putoamisen
   time.elapsed = now - time.start;
   if (time.elapsed > time.level) {
     time.start = now;
-    //luodaan uusi palikka aina kun palikka on laskeutunut
-    if (kentta.palikka.onPohjalla()) {
-      kentta.liimaaPalikkaKentalle();
-      kentta.luoUusiPalikka();
-      console.log("pohjal")
-    }else{
-      clear();
-      kentta.pudotaPalikka();
-    }
-    
+
+    clear();
+    kentta.pudotaPalikka(); //palikka putoaa koko olemassaolonsa aikana, kunnes se osuu johonkin
+
   }
   kentta.piirra(); //piirretään kenttä uudestaan päivitetyillä arvoilla
   requestId = requestAnimationFrame(animate);
 }
 
+const liikkeet = { //kaikki liikkeet, jotka käyttäjällä on käyettävissä. Näppäimet määritetelty vakioissa
+  
+  [KEY.VASEN]: p => ({ ...p, x: p.x - 1 }), //... kopio olion (shallow copy). tämä on uusi olio samoilla arvoilla. Eli kopioidaan p:hen palikka -olio, jonka arvoja muutetaan painetun näppäimen perusteella
+  [KEY.OIKEA]: p => ({ ...p, x: p.x + 1 }),
+  [KEY.ALAS]: p => ({ ...p, y: p.y + 1 }),
+  [KEY.YLOS]: p => kentta.rotate(p) //rotatoi palikan. Eli luo palikasta kopion, jota käänellään, ja palauttaa kopion.
+  
 
+};
 
 
 function pelaa() { //Aloittaa pelin. Sidottu "pelaa" -nappiin
   kentta.reset(); //alustetaan kenttä
 
-  
   time.start = performance.now();
   this.animate(); //looppi käyntiin
 
+  document.addEventListener('keydown', event => {
 
-  document.addEventListener("keydown", function (event) {
-
-    let palikka = kentta.palikka
-    //console.log(palikka.tyyppiId)
-    if (event.defaultPrevented) {
-      return;
-    }
-    if (event.code === "KeyS") { //liiku alaspäin
-      clear();
-      palikka.liiku("alas");
-
-    }
-    if (event.code === "KeyA") { //liiku vasemmalle
-      clear();
-      palikka.liiku("vasemmalle");
-    }
-    if (event.code === "KeyD") { //liiku oikealle
-      clear();
-      palikka.liiku("oikealle");
-    }
-    if (event.code === "KeyW") { //liiku oikealle
-
-      if (palikka.validiSiirto("rotate")) {
+    //liikkuminen tapahtuu luomalla palikka -oliosta kopio, ja muuttamalla VAIN kopion arvoja.
+    //Kopio liitetään kenttään, alkuperäinen häviää
+    if (liikkeet[event.code]) { //event.code = ennalta määritelty näppäin, esim. KEY.OIKEA
+      event.preventDefault();
+      
+      let p = liikkeet[event.code](kentta.palikka); //luodaan palikasta kopio, ja muokataan kopion arvoja 
+      if (kentta.valid(p)) {
         clear();
-        palikka.rotate(palikka);
-        //console.log("palikan päivitetty leveys",palikka.getTodellinenLeveys())
+        kentta.palikka.liiku(p); //p sisältää kopion palikka -oliosta, sekä painetun näppäimen tiedot, jotka lähetetään palikka -olion liiku() -metodille
+
       }
-
     }
-    event.preventDefault();
-    //console.log("palikan muoto",palikka.shape)
+  });
 
-    //console.log("kentän koko",SARAKKEET*BLOKIN_KOKO)
-  }, true);
 }
 
 
