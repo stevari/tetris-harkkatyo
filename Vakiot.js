f1d8d4b71a59cb8db8bfed4d5384d168548e722a
stevari <villevartia@hotmail.com>
2022-04-20 20:16:30 +0300

    kommentoitia

--- Vakiot.js
+++ Vakiot.js
@@ -7,32 +7,32 @@ const BLOKIN_KOKO = 30; //Määrittää yhden palikan koon canvasilla. Suurempi 
 
 const MUODOT = [ //Kaikki tetromiinojen muodot matriiseina. 0 = tyhjä paikka, numero = väritetty paikka
   [],
-  [[1, 1, 1, 1],
+  [[1, 1, 1, 1], //I
   [0, 0, 0, 0],
   [0, 0, 0, 0],
   [0, 0, 0, 0]],
 
   [[2, 0, 0],
-  [2, 2, 2],
+  [2, 2, 2], //J
   [0, 0, 0]],
 
   [[0, 0, 3],
-  [3, 3, 3],
+  [3, 3, 3],  //L
   [0, 0, 0]],
 
   [[4, 4],
-  [4, 4]],
+  [4, 4]], //O
 
   [[0, 5, 5],
-  [5, 5, 0],
+  [5, 5, 0], //S
   [0, 0, 0]],
 
   [[0, 6, 0],
-  [6, 6, 6],
+  [6, 6, 6], //T
   [0, 0, 0]],
 
   [[7, 7, 0],
-  [0, 7, 7],
+  [0, 7, 7], //Z
   [0, 0, 0]]
 ];
 
